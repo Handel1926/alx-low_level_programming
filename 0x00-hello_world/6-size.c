764b7f791cf6ed626c347f1c9440441effb45cab
Handel1926 <ebi1926a@gmail.com>
2022-10-13 05:17:09 -0700

    c-programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("size of an int: %d byte(s)\n", sizeof(b));
 	printf("size of a long int: %d byte(s)\n", sizeof(c));
 	printf("sive of a long long int: %d byte(s)", sizeof(d));
-	printf("size of a float: %d byte(s)", sizeof(e));
+	printf("size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
