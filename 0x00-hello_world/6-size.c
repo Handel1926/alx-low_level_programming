7e5c3035c4042a8f3de6534d5ea48bfbe094ce05
Handel1926 <ebi1926a@gmail.com>
2022-10-13 05:04:51 -0700

    low-level_programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the sizeof different var types
+  *
+  * Reaturn: Always 0 (Successful)
+  */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of a char: %d byte(s)\n", sizeof(a));
+	printf("size of an int: %d byte(s)\n", sizeof(b));
+	printf("size of a long int: %d byte(s)\n", sizeof(c));
+	printf("sive of a long long int: %d byte(s)", sizeof(d));
+	printf("size of a float: %d byte(s)", sizeof(e));
+	return (0);
+}
