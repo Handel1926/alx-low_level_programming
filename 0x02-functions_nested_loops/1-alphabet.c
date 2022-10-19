e55e280a5e04dcbab669ea50becc0a12e0aff98d
Handel1926 <ebi1926a@gmail.com>
2022-10-19 12:29:55 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabets
+ * Return: Always o (succsses)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n')
+}
+
